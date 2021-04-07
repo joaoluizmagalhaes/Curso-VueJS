59c0adb0a2f1614db6b8a849d6a4bc4b60076a3e
Magalhaes <joao.l.magalhaes@accenture.com>
2021-04-07 20:17:56 -0300

    Seção 6 - Aula 93.

---
+++ vue-components-intro/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <app-contador></app-contador>
+</template>
+
+<script>
+
+import AppContador from './AppContador.vue'
+
+export default {
+    components: {
+        'app-contador': AppContador
+    }
+}
+</script>
--- vue-components-intro/src/main.js
+++ vue-components-intro/src/main.js
@@ -1,58 +1,10 @@
 import Vue from 'vue'
 
-/*const data = {
-	titulo: 'Curso de VueJS',
-	contador: 0
-}*/
+import App from './App.vue'
+//import AppContador from './AppContador.vue'
 
-//Registrando o component globalmente
-/*Vue.component('jm-component', {
-	data: function() {
-		return {
-			titulo: 'Curso de VueJS',
-			contador: 0
-		}
-	},
-	methods: {
-		incrementar: function() {
-			this.contador++
-		}
-	},
-	template: `
-		<div>
-			<h1>{{ titulo }}</h1>
-			<button @click="incrementar">Clicado {{ contador }} vezes</button>
-		</div>
-	`
-}) */
-
-const jmComponent = {
-	data: function() {
-		return {
-			titulo: 'Curso de VueJS',
-			contador: 0
-		}
-	},
-	methods: {
-		incrementar: function() {
-			this.contador++
-		}
-	},
-	template: `
-		<div>
-			<h1>{{ titulo }}</h1>
-			<button @click="incrementar">Clicado {{ contador }} vezes</button>
-		</div>
-	`
-}
+//Vue.component('app-contador', AppContador)
 
 new Vue({
-	el: '#app1',
-	components: {
-		'jm-component' : jmComponent //registrado localmente
-	}
-})
-
-new Vue({
-	el: '#app2'
-})
+	render: h => h(App)
+}).$mount('#app')
