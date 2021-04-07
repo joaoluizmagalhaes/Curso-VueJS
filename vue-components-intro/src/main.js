dc8458d3904656f4770b356ac2cc4864d3355ebc
Magalhaes <joao.l.magalhaes@accenture.com>
2021-04-07 20:07:08 -0300

    Seção 6 - Aula 92 - Anterior era aula 91 eheh

--- vue-components-intro/src/main.js
+++ vue-components-intro/src/main.js
@@ -5,7 +5,8 @@ import Vue from 'vue'
 	contador: 0
 }*/
 
-Vue.component('jm-component', {
+//Registrando o component globalmente
+/*Vue.component('jm-component', {
 	data: function() {
 		return {
 			titulo: 'Curso de VueJS',
@@ -23,8 +24,35 @@ Vue.component('jm-component', {
 			<button @click="incrementar">Clicado {{ contador }} vezes</button>
 		</div>
 	`
+}) */
+
+const jmComponent = {
+	data: function() {
+		return {
+			titulo: 'Curso de VueJS',
+			contador: 0
+		}
+	},
+	methods: {
+		incrementar: function() {
+			this.contador++
+		}
+	},
+	template: `
+		<div>
+			<h1>{{ titulo }}</h1>
+			<button @click="incrementar">Clicado {{ contador }} vezes</button>
+		</div>
+	`
+}
+
+new Vue({
+	el: '#app1',
+	components: {
+		'jm-component' : jmComponent //registrado localmente
+	}
 })
 
 new Vue({
-	el: '#app'
+	el: '#app2'
 })
