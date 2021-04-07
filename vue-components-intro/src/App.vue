d41d597dd963ab965b257814a4b4df281cb3f6ab
Magalhaes <joao.l.magalhaes@accenture.com>
2021-04-07 20:29:36 -0300

    Seção 6 - Aula 94 - Boas Práticas.

--- vue-components-intro/src/App.vue
+++ vue-components-intro/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-    <app-contador></app-contador>
+    <div>
+        <AppHeader />
+        <AppContador />
+    </div>
 </template>
 
 <script>
 
+import AppHeader from './AppHeader.vue'
 import AppContador from './AppContador.vue'
 
 export default {
     components: {
-        'app-contador': AppContador
+        AppContador,
+        AppHeader
     }
 }
 </script>
