dd1b16ca7644ca4e25b3152cc08016dc3e6d37db
João <dukejlf@gmail.com>
2021-04-07 22:17:33 -0300

    Seção 7 - Aula 105.

--- vue-components-comunicacao/src/components/FilmesListaIten.vue
+++ vue-components-comunicacao/src/components/FilmesListaIten.vue
@@ -1,11 +1,19 @@
 <template>
     <li class="list-group-item">
-        <span>{{ filmeTitulo }}</span>
+        <span>{{ filmeTituloConcatenado }}</span>
         <button class="btn btn-success float-right">Editar</button>
     </li>
 </template>
 <script>
 export default {
-    props: ['filmeTitulo']
+    props: ['filmeTitulo'],
+    computed: {
+        filmeTituloConcatenado() {
+            return `Título: ${this.filmeTitulo}`
+        }
+    },
+    created() {
+        console.log('Filme título: ', this.filmeTitulo)
+    }
 }
 </script>
