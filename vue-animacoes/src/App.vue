b11764d0a8869ce884c8d21da38336e1d3d6a837
João <dukejlf@gmail.com>
2021-04-12 19:32:43 -0300

    Seção 10 - Aula 183, Animate CSS.

--- vue-animacoes/src/App.vue
+++ vue-animacoes/src/App.vue
@@ -12,9 +12,26 @@
 
       <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
       <transition name="fade">
-        <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
+        <div class="alert alert-primary" v-if="mostrar">Fade</div>
       </transition>
+      <transition name="zoom">
+        <div class="alert alert-primary" v-if="mostrar">Zoom</div>
+      </transition>
+      <transition name="slide" type="animation" :duration="{ enter: 1200, leave: 500 }">
+        <div class="alert alert-primary" v-if="mostrar">Slide</div>
+      </transition>
+
+      <hr>
+
+      <transition
+        enter-active-class="animate__animated animate__bounceInDown"
+        leave-active-class="animate__animated animate__bounceOutDown"
+      >
+        <div class="alert alert-primary" v-if="mostrar">Animate CSS</div>
+      </transition>
+
     </div>
+
 
   </div>
 </template>
@@ -29,7 +46,23 @@ export default {
 }
 </script>
 
+<style >
+  body {
+    overflow: hidden;
+  }
+</style>
+
 <style scoped>
+
+  .zoom-enter,
+  .zoom-leave-to {
+    transform: scale(0);
+  }
+
+  .zoom-enter-active,
+  .zoom-leave-active {
+    transition: transform 0.5s;
+  }
 
   .fade-enter,
   .fade-leave-to {
@@ -39,6 +72,31 @@ export default {
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 1s;
+  }
+
+  .slide-enter,
+  .slide-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active{
+    animation: slide 0.7s ease-in-out;
+    transition: opacity 1s;
+  }
+
+  .slide-leave-active {
+    animation: slide 0.7s reverse;
+    transition: opacity 1s;
+  }
+
+  @keyframes slide {
+    0% {
+      transform: translateX(-100px)
+    }
+
+    100% {
+      transform: translateX(0px);
+    }
   }
 
 
