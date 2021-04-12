ee79484f3b5f611e29930452a6d10aff82ee9967
João <dukejlf@gmail.com>
2021-04-12 18:46:34 -0300

    Seção 10 - Aula 176, animações.

---
+++ vue-animacoes/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transição/animação de elementos/components no Vue.</p>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
+      <transition name="fade">
+        <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
+      </transition>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mostrar: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 1s;
+  }
+
+
+</style>
