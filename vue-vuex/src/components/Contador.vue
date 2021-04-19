ee93613cd43ba5b8d535d7f736f9983cda4717c3
João <dukejlf@gmail.com>
2021-04-19 10:03:21 -0300

    Seção 13 - Aula 295, Vuex.

---
+++ vue-vuex/src/components/Contador.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+        <h4 class="font-weight-light">Contador: {{ contadorAlias }}</h4>
+        <h4 class="font-weight-light">Contador Multiplcado: {{ contadorMultiplicado }}</h4>
+        <button 
+            class="btn btn-lg btn-danger mr-2"
+            @click="decrementar"    
+        >
+            -
+        </button>
+        <button 
+            class="btn btn-lg btn-success"
+            @click="incrementar"
+        >
+            +
+        </button>
+    </div>
+</template>
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    data() {
+        return {
+            contadorLocal: 2
+        }
+    },
+    /*computed: mapState(['contador']),
+    computed: mapState({
+        //contador: state => state.contador
+        contador: 'contador',
+        contadorAlias: 'contador',
+        contadorMultiplicado(state) {
+            return state.contador * this.contadorLocal
+        }
+    }),*/
+    computed: {
+        ...mapState({
+            //contador: state => state.contador
+            contador: 'contador',
+            contadorAlias: 'contador',
+            contadorMultiplicado(state) {
+                return state.contador * this.contadorLocal
+            }
+        })
+    },
+    methods: {
+        decrementar() {
+            this.$store.state.contador--
+        },
+        incrementar() {
+            this.$store.state.contador++
+        }
+    }
+}
+</script>
